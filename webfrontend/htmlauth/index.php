375ed5b94046376f7d755005fd9da8e11303be30
Martin Wimmer <martin@wimmer.dev>
2025-01-19 21:03:57 +0100

    Remove outdated templates and implement dynamic localization
    Replaced static templates and language files with a more dynamic approach using a PHP-based localization system. This ensures better maintainability and allows more flexible language support. Old HTML and TXT templates have been removed and replaced with `.ini` language files integrated into the PHP framework.

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,8 +1,26 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
- </body>
-</html>
+<?php
+
+require_once "loxberry_io.php";
+require_once "Config/Lite.php";
+require_once "loxberry_web.php";
+require_once "loxberry_log.php";
+
+// This will read your language files to the array $L
+$L = LBSystem::readlanguage("language.ini");
+$template_title = "Luxtronik 2";
+$helplink = "http://www.loxwiki.eu/display/LOXBERRY/Luxtronik2";
+$helptemplate = "help.html";
+
+LBWeb::lbheader($template_title, $helplink, $helptemplate);
+
+// This is the main area for your plugin
+?>
+
+  <p><?=$L['TOP.ALPHA']?></p>
+  <p><?=$L['TOP.BETA']?></p>
+  <p><?=$L['BOTTOM.GAMMA']?></p>
+
+<?php
+// Finally print the footer
+LBWeb::lbfooter();
+?>
